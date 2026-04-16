d1fd94d31db4f4dbf3b50180767ccafd15617b1f
DiegoDev <diegorego516@gmail.com>
2026-04-16 19:35:42 -0300

    Update ip_recon.py

--- ip_recon.py
+++ ip_recon.py
@@ -16,14 +16,6 @@ BANNER = r"""
 
 print(BANNER)
 
-print("\nIP Recon Menu")
-options = input("1. Recon The Target IP? (Y/N): ")
-options = input("2. Scan The Target IP For Open_Ports? (Y/N): ")
-options = input("3. Verify Target IP For Vulnerabilities? (Y/N): ")
-options = input("4. Check IF there IS Any Vulnerabilities In The Target IP? (Y/N): ")
-options = input("5. Exit? (Y/N): ")
-sys.exit(0)
-print("Exiting...")
 
 print("\nIP Recon Options Menu")
 options = input("1. Scan The Target IP? (Y/N): ")
@@ -31,6 +23,5 @@ options = input("2. Verify If There Are Any Vulnerabilities In The Target Operat
 options = input("3. Analyze For Any Potential Vulnerabilities In The Target Networks? (Y/N): ")
 options = input("4. Generate A Concise, Convincing And Promissing Well Detailed Bug Bounty Report Document? (Y/N): ")
 options = input("5. Exit? (Y/N): ")
-sys.exit(0)
 print("Exiting...")
-
+sys.exit(0)
