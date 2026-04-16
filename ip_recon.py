be24eedd3ed3216e4b57372c1af3375c930f1f6d
DiegoDev <diegorego516@gmail.com>
2026-04-16 17:44:51 -0300

    Add files via upload

---
+++ ip_recon.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import requests
+
+
+
+BANNER = r"""
+██╗██████╗     ██████╗ ███████╗ ██████╗ ██████╗ ███╗   ██╗
+██║██╔══██╗    ██╔══██╗██╔════╝██╔════╝██╔═══██╗████╗  ██║
+██║██████╔╝    ██████╔╝█████╗  ██║     ██║   ██║██╔██╗ ██║
+██║██╔═══╝     ██╔══██╗██╔══╝  ██║     ██║   ██║██║╚██╗██║
+██║██║         ██║  ██║███████╗╚██████╗╚██████╔╝██║ ╚████║
+╚═╝╚═╝         ╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝
+		[ IP RECON V3 - BY DIEGODEV ]
+"""
+
+print(BANNER)
+
+print("\nIP Recon Menu")
+options = input("1. Recon The Target IP? (Y/N): ")
+options = input("2. Scan The Target IP For Open_Ports? (Y/N): ")
+options = input("3. Verify Target IP For Vulnerabilities? (Y/N): ")
+options = input("4. Check IF there IS Any Vulnerabilities In The Target IP? (Y/N): ")
+options = input("5. Exit? (Y/N): ")
+self = sys.exit(0)
+print("Exiting...")
+
+print("\nIP Recon Options Menu")
+options = input("1. Recon The IP Addresses '127.0.0.1, 192.168.87.145, 168.145.62.127'? (Y/N): ")
+options = input("2. Scan The Target Operating Systems For Open_Ports '1-65535'? (Y/N): ")
+options = input("3. Analyze The Target Operating Systems For Vulnerabilities? (Y/N): ")
+options = input("4. Verify The Target Devices For Possible Vulnerabilities? (Y/N): ")
+options = input("5. Exit? (Y/N: ")
+
+
+def ip_recon(IPv4, Windows11, scan_ip_addresses):
+
+	def recon_target_operating_system():
+      		ip_recon = IPv4 > recon_target_operating_system >= scan_ip_addresses
+ip_recon ("IPv4", "Windows11", "127.0.0.1", "192.168.87.145", "168.145.62.127")
+
+def scan_ip_addresses (IPv4):
+
+	def Windows11():
+
+		scan_ip_addresses > IPv4 > Windows11 >= scan_ip_addresses
+	scan_ip_addresses ("IPv4", "Windows11", "scan_ip_addresses")
+
+def scan_for_target_ip_addresses (IPv4, Windows11, scan_ip_addresses):
+
+	def get_all_files(modules):
+
+		options = input("Generate the result of the ip that you have done the recon with it and the target operating system.")
+
+if input == "Y":
+		print("You have done the recon of the IP Addresses and the target operating system.")
+
+def ip_recon = recon.ip_addresses("Do you want to start recon_scans on known target ip addresses? (Y/N): E.G., '127.0.0.1', '192.168.87.145', '168.145.62.127': Enter here the Target IP that you want to run the ip recon_scan into it "):
+
+if input == "Y":
+	        print("You have sucessfully scanned the target ip addresses.")
